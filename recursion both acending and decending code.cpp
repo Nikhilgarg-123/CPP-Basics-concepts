6ec8a0c55caff65f229727867985093c3f526677
Nikhil Garg <73421482+Nikhilgarg-123@users.noreply.github.com>
2022-08-10 17:19:50 +0530

    Update recursion both acending and decending code.cpp

--- recursion both acending and decending code.cpp
+++ recursion both acending and decending code.cpp
@@ -3,9 +3,9 @@ using namespace std;
 
 void fun(int n){
     if(n>0){
-        // cout<<n;
+        // cout<<n;    //head 
         fun(n-1);
-        cout<<n;
+        cout<<n;        // tail
 
     }
 }
