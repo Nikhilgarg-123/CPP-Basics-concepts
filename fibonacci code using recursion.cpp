ccc4e50df377089712ec74e6b7dafa5568b5763f
Nikhil Garg <73421482+Nikhilgarg-123@users.noreply.github.com>
2022-08-12 21:21:38 +0530

    Add files via upload

---
+++ fibonacci code using recursion.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int fibonacci(int n){
+    if (n==0){
+        return 0;
+    }
+    else if (n==1){
+        return 1;
+    }
+    else{
+        return fibonacci(n-1)+fibonacci(n-2);
+    }
+}
+
+int main() {
+    cout<<"Hello Nikhil!"<<endl;
+    cout<<fibonacci(6);
+}
