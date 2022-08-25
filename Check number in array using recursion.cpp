bde2c7a36e74f473b2fae3bb74d6e02ec05b6e88
Nikhil garg <nikhilgarg.drb@gmail.com>
2022-08-26 00:38:00 +0530

    Check number in array using recursion

--- Check number in array using recursion.cpp
+++ Check number in array using recursion.cpp
@@ -6,15 +6,17 @@ bool find_number(int arr[],int size,int x){
     return true;
   }
   if (size==0){
-    return 0;
+    return false;
   }
   else{
     return find_number(arr+1 ,size-1,x);
   }
 }
 
+
 int main(){
   int n;
+  cout<<"number of element:";
   cin>>n;
   int arr[n];
   for (int i=0;i<n;i++){
