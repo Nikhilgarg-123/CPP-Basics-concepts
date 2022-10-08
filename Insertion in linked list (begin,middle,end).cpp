6732b73fcc3c6b497b90f200d2b3a7b084e05acf
Nikhil Garg <73421482+Nikhilgarg-123@users.noreply.github.com>
2022-10-08 15:28:05 +0530

    Insertion at linked list

---
+++ Insertion in linked list (begin,middle,end).cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+using namespace std;
+class Node{
+    public:
+        int data;
+        Node * next;
+        Node(int x){
+            data =x;
+            next=NULL;
+        }
+};
+
+
+
+void insertatbegin(Node*&head, int x){
+    if (head==NULL){
+        head= new Node(x);
+        return;
+    }
+    Node *n= new Node(x);
+    n->next= head;
+    head= n;
+}
+
+
+
+int length(Node*head){
+    int count=0;
+    while(head!=NULL){
+        count++;
+        head=head->next;
+    }
+    return count;
+}
+
+
+
+void insesrtatend(Node*&head,int x){
+    if(head==NULL){
+        head= new Node(x);
+        return;
+    }
+
+    Node* temp= head;
+    while(temp->next!=NULL){
+        temp=temp->next;
+    }
+    
+    Node*n= new Node(x);
+    temp->next= n;
+    return;
+
+}
+
+
+
+void insertinmiddle(Node*&head,int d, int p){
+    if(p==0 or head==NULL){
+        insertatbegin(head,d);
+    }
+    else  if(p>length(head)){
+        insesrtatend(head,d);
+    }
+
+    else{
+        int jump=1;
+        Node*temp=head;
+        while(jump<p-1){
+            temp=temp->next;
+            jump++;
+        }
+        Node* n= new Node(d);
+        n->next=temp->next; 
+        temp->next= n;
+
+    }
+
+    }
+
+void print(Node *head){
+    while(head!=NULL){
+        cout<<head->data << "->";
+        head=head->next;
+    }
+}
+
+int main() {
+Node *head= NULL;
+insertatbegin(head,30);
+insertatbegin(head,20);
+insertatbegin(head,10);
+insesrtatend(head,90);
+insesrtatend(head,100);
+insertinmiddle(head,40,4);
+insertinmiddle(head,50,5);
+insertinmiddle(head,60,6);
+insertinmiddle(head,70,7);
+insertinmiddle(head,80,8);
+
+print(head);
+}
